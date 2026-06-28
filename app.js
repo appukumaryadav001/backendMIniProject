0ca00540b714f81fd990739b10d15516450a627d
Appukumaryadav001 <appukumaryadav19@gmail.com>
2026-06-28 22:39:27 +0530

    basic code

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const app = express();
+const userModel = require("./models/user");
+const cookieParser = require("cookie-parser");
 
+
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cookieParser());
 app.get("/",(req,res)=>{
-    res.send("appu")
+    res.render("index")
 })
 app.listen(3000,()=>{
     console.log("Server start from 3000");
---
+++ models/user.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://127.0.0.1:27017/miniproject");
+
+const userSchema = new mongoose.Schema({
+   username:{type:String},
+   name:{type:String},
+   age:{type:Number},
+   email:{type:String},
+   password:{String}
+},{timestamps:true});
+
+
+module.exports = mongoose.model("User",userSchema);
